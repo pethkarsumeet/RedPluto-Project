d0dc472b99251610d0b565dd126cb687c90d6c69
pethkarsumeet <Pethkarsumeet123@gmail.com>
2025-06-19 17:10:39 +0530

    basename added

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   const [students, setStudents] = useState([]);
 
   return (
-    <Router>
-      <Routes>
+    <Router  basename="/RedPluto-Project/">
+      <Routes >
         <Route path="/" element={<IndexPage students={students} setStudents={setStudents} />} />
         <Route path="/create" element={<CreatePage students={students} setStudents={setStudents} />} />
         <Route path="/edit/:id" element={<EditPage students={students} setStudents={setStudents} />} />
